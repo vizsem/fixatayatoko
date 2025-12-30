e83c1c997ded86b3dcc95a0c2dcb32b43a7e4b34
vizsem <git@github.com:vizsem/fixatayatoko.git>
2025-12-31 05:33:16 +0700

    fix:  prerendering for admin pages

--- src/app/admin/inventory/page.tsx
+++ src/app/admin/inventory/page.tsx
@@ -1,6 +1,8 @@
+// ✅ Nonaktifkan prerendering — hanya render di client
 // src/app/(admin)/inventory/page.tsx
 'use client';
 
+export const dynamic = 'force-dynamic';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { auth } from '@/lib/firebase';
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+ NEXT_PUBLIC_FIREBASE_API_KEY=AIzaSyDV5Oz_zphv8UatLlZssdLkrbHSIZ8fOZI
+NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN=atayatoko2.firebaseapp.com
+NEXT_PUBLIC_FIREBASE_PROJECT_ID=atayatoko2
+NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET=atayatoko2.firebasestorage.app
+NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID=405021515196
+NEXT_PUBLIC_FIREBASE_APP_ID=1:405021515196:web:3693ba57b852525e435fb9
+NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID=G-6TPZQ4KX1K
+
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
